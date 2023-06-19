443953bf634115beadb0021c672da3ce82b81152
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-19 22:53:47 +0300

    sending Login Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresnIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -23,17 +24,30 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(error => {
-            let errorMessage = 'An unknown error occured!';
-            if(!error.error || !error.error.error) {
-                return throwError(errorMessage);
-            }
+        )
+        .pipe(
+            catchError(error => {
+                let errorMessage = 'An unknown error occured!';
+                if(!error.error || !error.error.error) {
+                    return throwError(errorMessage);
+                }
 
-            switch (error.error.error.message) {
-                case 'EMAIL_EXISTS':
-                  errorMessage = 'This email exists already';
-              }
-              return throwError(errorMessage);
-        }));
+                switch (error.error.error.message) {
+                    case 'EMAIL_EXISTS':
+                    errorMessage = 'This email exists already';
+                }
+                return throwError(errorMessage);
+            })
+        );
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCUJdtEXIvVgfl4leR-oF82I3OE9vwoIMM', 
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        );
     }
 }
